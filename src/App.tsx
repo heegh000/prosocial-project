e6716f128dda24ab22c32d254c4b7b6040d9b050
dewpe000 <dewpe000@naver.com>
2023-05-11 10:33:40 +0900

    AddPost initial implementation without HTTP request

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import './App.css';
+import { AddPost } from "./components/AddPost";
+import { ViewPost } from "./components/ViewPost";
+import { Footer } from "./components/Footer";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <AddPost></AddPost>
+        <ViewPost></ViewPost>
+        <Footer></Footer>
+        <Routes>
+        </Routes>
+
+      </Router>
     </div>
   );
 }
